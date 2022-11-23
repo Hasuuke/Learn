67605c2b189283055e7dd9720b2f2988ab6d3bb4
Hasuuka <enricot.t@web.de>
2022-11-23 16:13:44 +0700

    Reorganized Packages

---
+++ app/src/main/java/vn/tdtu/mad/learn/Screens/TaskActivity.java
@@ -0,0 +1,14 @@
+package vn.tdtu.mad.learn.Screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import vn.tdtu.mad.learn.R;
+
+public class TaskActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_task);
+    }
+}
