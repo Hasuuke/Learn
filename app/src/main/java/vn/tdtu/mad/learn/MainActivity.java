2d4de44daf277c40cfb9c3ddd0a9a334f9f74479
tamkhojj <tamkhoitran2002@gmail.com>
2022-11-25 18:57:17 +0700

    Design Homepage UI

--- app/src/main/java/vn/tdtu/mad/learn/MainActivity.java
+++ app/src/main/java/vn/tdtu/mad/learn/MainActivity.java
@@ -1,13 +1,19 @@
 package vn.tdtu.mad.learn;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import android.graphics.Color;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+        TextView textView;
+        ImageView imageView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
     }
 }
