02c42a6741622b360064a54b6bd375b6e8666bd8
tamkhojj <tamkhoitran2002@gmail.com>
2022-12-07 18:57:31 +0700

    Design main screen

---
+++ app/src/main/java/vn/tdtu/mad/learn/Screens/VideoActivity.java
@@ -0,0 +1,16 @@
+package vn.tdtu.mad.learn.Screens;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import vn.tdtu.mad.learn.R;
+
+public class VideoActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.video);
+    }
+}
